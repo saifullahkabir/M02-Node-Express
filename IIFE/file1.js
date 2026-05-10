c75a3e27a351da7b6ca7d522963a34e2672c4dba
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-10 21:09:20 +0600

    harnessing IIFE

---
+++ IIFE/file1.js
@@ -0,0 +1,13 @@
+// let a = 10;
+// let a = 20;
+
+(() => {
+  let a = 10;
+  console.log(a);
+})();
+
+(() => {
+  let a = 20;
+  console.log(20);
+})();
+
---
+++ IIFE/file2.js
@@ -0,0 +1,7 @@
+(function f1() {
+  console.log("Rahim");
+})();
+
+(function f1() {
+  console.log("Karim");
+})();
