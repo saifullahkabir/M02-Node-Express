bb9316a20f148eb4fdd393b52018a064849be48a
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-11 17:34:49 +0600

    get single product controller implement

--- Learning_Node/src/controller/products.controller.ts
+++ Learning_Node/src/controller/products.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProducts } from "../service/products.service";
+import type { IProducts } from "../types/products.types";
 
 export const productsController = (
   req: IncomingMessage,
@@ -8,6 +9,13 @@ export const productsController = (
   const url = req.url;
   const method = req.method;
 
+  const urlParts = url?.split("/");
+  //   console.log(urlParts);
+
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+
+  //* get all products
   if (url === "/products" && method === "GET") {
     // const products = [
     //   {
@@ -16,13 +24,25 @@ export const productsController = (
     //   },
     // ];
 
-    const products =readProducts();
+    const products = readProducts();
 
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
         message: "Products retrived successfully",
         data: products,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    //* get single product
+    const products = readProducts();
+    const product = products.find((p: IProducts) => p.id === id);
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product retrived successfully",
+        data: product,
       }),
     );
   }
