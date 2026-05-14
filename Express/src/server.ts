091129556a13272edcb6228a85c5cac34f8ef005
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-15 00:17:10 +0600

    executing pool and creating tables

--- Express/src/server.ts
+++ Express/src/server.ts
@@ -17,6 +17,28 @@ const pool = new Pool({
   connectionString: config.connection_string,
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
+
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Express server is running");
   res.status(200).json({
