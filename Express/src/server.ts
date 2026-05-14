b5914bed3ac71c1c0b09d1a70188975487c3c1b8
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-15 01:29:58 +0600

    creating user with post method using postgresql

--- Express/src/server.ts
+++ Express/src/server.ts
@@ -22,9 +22,9 @@ const initDB = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users(
       id SERIAL PRIMARY KEY,
-      name VARCHAR(20),
-      email VARCHAR(20) NOT NULL,
-      password VARCHAR(20) NOT NULL,
+      name VARCHAR(50),
+      email VARCHAR(50) UNIQUE NOT NULL,
+      password VARCHAR(50) NOT NULL,
       is_active BOOLEAN DEFAULT true,
       age INT,
       created_at TIMESTAMP DEFAULT NOW(),
@@ -48,15 +48,29 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
+  try {
+    const { name, email, password, age } = req.body;
 
-  res.status(201).json({
-    message: "Created Successfully",
-    data: {
-      name,
-      email,
-    },
-  });
+    const result = await pool.query(
+      `
+    INSERT INTO  users(name, email, password, age) VALUES($1, $2, $3, $4) 
+    RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      success: true,  
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      sucess: false,
+      message: error.message,
+      error: error,
+    });
+  }
 });
 
 app.listen(port, () => {
