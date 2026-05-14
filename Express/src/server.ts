99c30f3c3465b290d7dcc058e9f399619e14f516
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-14 22:43:42 +0600

    implement config

--- Express/src/server.ts
+++ Express/src/server.ts
@@ -1,9 +1,40 @@
-import express from "express"
-const app = express();
-const port = 5000;
+import express, {
+  application,
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+import config from "./config";
 
-app.get("/", (req, res) => {
-  res.send("Express server is running");
+const app: Application = express();
+const port = config.port;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+app.get("/", (req: Request, res: Response) => {
+  //   res.send("Express server is running");
+  res.status(200).json({
+    message: "Express Server",
+    author: "Saifullah Kabir",
+  });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  res.status(201).json({
+    message: "Created Successfully",
+    data: {
+      name,
+      email,
+    },
+  });
 });
 
 app.listen(port, () => {
