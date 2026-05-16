f675b9cfae393343380db8ed65e8b4c49289c43b
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-05-16 21:33:36 +0600

    creating user profiles

--- Express/src/app.ts
+++ Express/src/app.ts
@@ -5,6 +5,7 @@ import express, {
 } from "express";
 import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app: Application = express();
 
@@ -20,5 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRoute);
+
+app.use("/api/profile", profileRoute);
 
 export default app;
